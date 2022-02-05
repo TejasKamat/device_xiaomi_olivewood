e28aa2ea3baa4d8101228670fed01622154569de
Alexander Koskovich <akoskovich@pm.me>
2022-02-05 22:31:47 +0100

    olivewood: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -141,7 +140,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libmm-qcamera \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
