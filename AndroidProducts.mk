37d71817a39a4ce5b894a82d4b63098d50505a60
TejasKamat <kamattejas23@gmail.com>
2022-06-01 20:01:01 +0530

    olivewood: It's snow time
    * Evolution-X
    
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_olivewood.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_olivewood-user \
-    lineage_olivewood-userdebug \
-    lineage_olivewood-eng
+    $(LOCAL_DIR)/evolution_olivewood.mk
--- evolution_olivewood.mk
+++ evolution_olivewood.mk
@@ -8,24 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Set Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 28
-
-#
 # All components inherited here go to system_ext image
-#
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 # Inherit from olivewood device
 $(call inherit-product, device/xiaomi/olivewood/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := olivewood
-PRODUCT_NAME := lineage_olivewood
+PRODUCT_NAME := evolution_olivewood
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Redmi 8A/8A Dual
 PRODUCT_MANUFACTURER := Xiaomi
@@ -33,9 +28,3 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olivewood-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/olivewood/olivewood:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
